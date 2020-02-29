b6c17acb52a93a17272da3ba4a5d17050db0839c
alexknipfer <alexanderknipfer@gmail.com>
2020-02-28 22:30:05 -0500

    Setup theme

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    textColor: theme => ({
+      ...theme('colors'),
+      primaryGray: '#443a3a',
+    }),
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      primaryGray: '#443a3a',
+    }),
+  },
+};
