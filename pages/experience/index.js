b37064cae2926ff7612935479e70eb3b89e22df6
alexknipfer <alexanderknipfer@gmail.com>
2020-03-01 00:01:01 -0500

    Add experience page

--- pages/experience/index.js
+++ pages/experience/index.js
@@ -1,9 +1,18 @@
 import DefaultLayout from '../../components/default-layout';
+import Careers from '../../components/experience/careers';
+import Education from '../../components/experience/education';
+import Technologies from '../../components/experience/technologies';
 
 const Experience = () => {
   return (
     <DefaultLayout>
-      <h3>Experience page</h3>
+      <div className="flex justify-center items-center h-full">
+        <div className="grid grid-cols-1 col-auto">
+          <Careers />
+          <Education />
+          <Technologies />
+        </div>
+      </div>
     </DefaultLayout>
   );
 };
