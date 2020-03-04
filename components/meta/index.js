adc593518ce0bddaa7eccb8971d2e9c594a0dbcd
alexknipfer <alexanderknipfer@gmail.com>
2020-03-03 21:17:17 -0500

    Add meta component

---
+++ components/meta/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default ({ title, description, url, keywords }) => (
+  <Head>
+    {title && <meta name="og:title" content={title} />}
+    {url && <meta name="og:url" content={url} />}
+    {description && <meta name="description" content={description} />}
+    {description && <meta name="og:description" content={description} />}
+    {keywords && <meta name="keywords" content={keywords} />}
+  </Head>
+);
