ce5e4acb1811735fdafb9659324f9b08255226ad
alexknipfer <alexanderknipfer@gmail.com>
2020-03-04 21:29:43 -0500

    Add projects to page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,9 +1,39 @@
 import DefaultLayout from '../../components/default-layout';
+import Card from '../../components/projects/card';
+
+const projects = [
+  {
+    name: 'Botsai',
+    link: 'https://botsai.com',
+    img: '/static/images/projects/botsai_project.png',
+  },
+  {
+    name: 'Ascendum',
+    link: 'https://ascendum.com',
+    img: '/static/images/projects/ascendum_project.png',
+  },
+  {
+    name: 'NPM Slackbot',
+    link: 'https://github.com/alexknipfer/npm-bot',
+    img: '/static/images/projects/npm_bot_project.png',
+  },
+  {
+    name: 'Wanna see more?',
+    link: 'https://github.com/alexknipfer',
+    img: '/static/images/projects/github_project.png',
+  },
+];
 
 const Projects = () => {
   return (
     <DefaultLayout>
-      <h3>My Projects</h3>
+      <div className="flex justify-center items-center h-full">
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+          {projects.map((p, i) => (
+            <Card name={p.name} link={p.link} img={p.img} key={i} />
+          ))}
+        </div>
+      </div>
     </DefaultLayout>
   );
 };
