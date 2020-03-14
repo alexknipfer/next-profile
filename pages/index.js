09615fc904fef6dc2674f6265f9d438560351377
alexknipfer <alexanderknipfer@gmail.com>
2020-03-13 21:52:16 -0400

    Update home page description

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Home = () => {
     <DefaultLayout>
       <Meta
         title="Alex Knipfer - Full Stack Developer from Cincinnati"
-        description="Talented full stack developer from Cincinnati, Ohio"
+        description="Alex Knipfer - Specializes in building web applications using tools such as React, Redux, Angular, GraphQL, MongoDB, and more. When I am not working, you will find me working and contributing to multiple open source projects (GitHub: github.com/alexknipfer) or getting some late night chess games in on Chess.com!"
         url="https://alexknipfer.com"
       />
       <Welcome />
