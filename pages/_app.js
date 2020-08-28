d0496e549e1b09a76280888ee24369dfd1fe54d2
Alex <alexanderknipfer@gmail.com>
2020-08-27 20:50:10 -0400

    Update viewport meta to prevent warning

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import App from 'next/app';
+import Head from 'next/head';
+
 import '../css/tailwind.css';
 import '../css/theme.css';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
+
+    return (
+      <Fragment>
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Fragment>
+    );
   }
 }
 
