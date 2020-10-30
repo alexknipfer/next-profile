0094a2d7997cf08fc9506d7d1a38579db9d8e2f0
Alex <alexanderknipfer@gmail.com>
2020-10-29 22:09:23 -0400

    Setup models

---
+++ interfaces/Prismic.ts
@@ -0,0 +1,10 @@
+export interface PrismicImage {
+  alt: string;
+  dimensions: PrismicImageDimensions;
+  url: string;
+}
+
+export interface PrismicImageDimensions {
+  width: number;
+  height: number;
+}
