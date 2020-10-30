3555df4987b711c938b97efa37848afdbd592b79
Alex <alexanderknipfer@gmail.com>
2020-10-29 22:09:42 -0400

    Wire up apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,29 @@
 import React, { Fragment } from 'react';
-import App from 'next/app';
+import { ApolloProvider } from '@apollo/client';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
+
+import { useApollo } from '../lib/apolloClient';
 
 import '../css/tailwind.css';
 import '../css/theme.css';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const apolloClient = useApollo(pageProps.initialApolloState);
 
-    return (
-      <Fragment>
-        <Head>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />
-        </Head>
+  return (
+    <Fragment>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+      </Head>
+      <ApolloProvider client={apolloClient}>
         <Component {...pageProps} />
-      </Fragment>
-    );
-  }
-}
+      </ApolloProvider>
+    </Fragment>
+  );
+};
 
-export default MyApp;
+export default App;
--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,34 +1,28 @@
 import { useTrail, animated } from 'react-spring';
+import { gql, useQuery } from '@apollo/client';
+import { GetStaticProps } from 'next';
 
 import DefaultLayout from '../../components/default-layout';
 import Meta from '../../components/meta';
+import { initializeApollo } from '../../lib/apolloClient';
+import { ContactPageResponse } from '../../interfaces/Contact';
 
-const contactMethods = [
-  {
-    img: '/static/images/contact/gmail_logo.svg',
-    alt: 'gmail contact',
-    href: 'mailto:alexanderknipfer@gmail.com',
-    text: 'alexanderknipfer@gmail.com',
-  },
-  {
-    img: '/static/images/contact/linkedin_logo.svg',
-    alt: 'linkedin contact',
-    href: 'https://www.linkedin.com/in/alex-knipfer-4381a6115/',
-    text: 'linkedin.com/alexknipfer',
-  },
-  {
-    img: '/static/images/contact/github_logo.svg',
-    alt: 'github contact',
-    href: 'https://github.com/alexknipfer',
-    text: 'github.com/alexknipfer',
-  },
-  {
-    img: '/static/images/contact/twitter_logo.svg',
-    alt: 'twitter contact',
-    href: 'https://twitter.com/knipferalex',
-    text: 'twitter.com/knipferalex',
-  },
-];
+const CONTACT_QUERY = gql`
+  query {
+    contact_page(uid: "contact-page", lang: "en-us") {
+      contact_link_meta {
+        contact_link_image
+        link_text
+        link_href {
+          __typename
+          ... on _ExternalLink {
+            url
+          }
+        }
+      }
+    }
+  }
+`;
 
 const config = {
   mass: 5,
@@ -37,12 +31,20 @@ const config = {
 };
 
 const Contact = () => {
+  const { data, loading } = useQuery<ContactPageResponse>(CONTACT_QUERY);
+
+  const contactMethods = data.contact_page.contact_link_meta;
+
   const trail = useTrail(contactMethods.length, {
     config,
     opacity: 1,
     x: 0,
     from: { opacity: 0, x: 40 },
   });
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
 
   return (
     <DefaultLayout>
@@ -54,23 +56,26 @@ const Contact = () => {
       <div className="flex flex-col justify-center items-center h-full p-8 md:p-20">
         <div className="grid grid-cols-1 col-auto">
           {trail.map(({ x, ...rest }, index) => (
-            <div className="lg:transform lg:hover:scale-110 lg:transition lg:duration-500 lg:ease-in-out" key={index}>
+            <div
+              className="lg:transform lg:hover:scale-110 lg:transition lg:duration-500 lg:ease-in-out"
+              key={index}
+            >
               <animated.a
                 className="flex items-center mb-6"
-                href={contactMethods[index].href}
+                href={contactMethods[index].link_href.url}
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{
                   ...rest,
-                  transform: x.interpolate(x => `translate3d(0, ${x}px, 0)`),
+                  transform: x.interpolate((x) => `translate3d(0, ${x}px, 0)`),
                 }}
               >
                 <img
-                  src={contactMethods[index].img}
-                  alt={contactMethods[index].alt}
+                  src={contactMethods[index].contact_link_image.url}
+                  alt={contactMethods[index].contact_link_image.alt}
                   className="mr-4 w-16"
                 />
-                <span>{contactMethods[index].text}</span>
+                <span>{contactMethods[index].link_text}</span>
               </animated.a>
             </div>
           ))}
@@ -78,6 +83,20 @@ const Contact = () => {
       </div>
     </DefaultLayout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: CONTACT_QUERY,
+  });
+
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+  };
 };
 
 export default Contact;
