0cc41d8d1298aeefe866d272bac6a3b308cc4db5
Alex <alexanderknipfer@gmail.com>
2020-11-06 20:31:41 -0500

    Update theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
         </Head>
-        <body>
+        <body className="theme-light">
           <Main />
           <NextScript />
         </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       },
       colors: {
         background: {
+          navigation: 'var(--bg-background-navigation)',
+          'navigation-hover': 'var(--bg-background-navigation-hover)',
           primary: 'var(--bg-background-primary)',
           text: 'var(--bg-background-text)',
           overlay: 'var(--bg-background-overlay)',
