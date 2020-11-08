f9e6c27b9e025ddae8906bf173199ffb5dfb1e29
Alex <alexanderknipfer@gmail.com>
2020-11-07 21:13:20 -0500

    Add svgr

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ module.exports = {
       require('./scripts/generate-sitemap');
     }
 
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
+
     return config;
   },
   images: {
