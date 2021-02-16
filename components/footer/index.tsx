72c6edbb5f389d540d1180079dbc838572575882
Alex <alexanderknipfer@gmail.com>
2021-02-16 16:11:08 -0500

    Add aria labels on social links

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -9,23 +9,28 @@ const socialAccounts = [
   {
     link: 'https://twitter.com/knipferalex',
     icon: TwitterIcon,
+    label: "Alex Knipfer's Twitter profile",
   },
   {
     link: 'https://github.com/alexknipfer',
     icon: GithubIcon,
+    label: "Alex Knipfer's GitHub profile",
   },
   {
     link: 'https://instagram.com/alexknipfer',
     icon: InstagramIcon,
+    label: "Alex Knipfer's Instagram profile",
   },
   {
     link: 'https://www.linkedin.com/in/alex-knipfer-4381a6115/',
     icon: LinkedInIcon,
+    label: "Alex Knipfer's LinkedIn profile",
   },
   {
     link:
       'mailto:alexanderknipfer@gmail.com?subject=Reaching out from alexknipfer.com',
     icon: EmailIcon,
+    label: 'Send email to alexanderknipfer@gmail.com',
   },
 ];
 
@@ -34,7 +39,7 @@ const Footer: React.FC = () => {
     <footer className="flex flex-col items-center py-6">
       <NowPlaying />
       <div className="flex justify-around mt-4 w-64">
-        {socialAccounts.map(({ link, icon }) => {
+        {socialAccounts.map(({ link, icon, label }) => {
           const Icon = icon;
 
           return (
@@ -43,6 +48,7 @@ const Footer: React.FC = () => {
               href={link}
               target="_blank"
               rel="noopener noreferrer"
+              aria-label={label}
               className="hover:bg-gray-100 dark:hover:bg-gray-800 p-3 rounded transition duration-300 text-gray-700 dark:text-gray-200"
             >
               <Icon width={20} height={20} className="fill-current" />
