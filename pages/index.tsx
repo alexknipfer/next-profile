6e119843836f44fe7bb72010f2522a699429a0a8
Alex <alexanderknipfer@gmail.com>
2021-02-16 16:11:54 -0500

    Update meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const Home = () => {
     <DefaultLayout>
       <Meta
         title="Alex Knipfer - Full Stack Developer from Cincinnati"
-        description="Alex Knipfer - Specializes in building web applications using tools such as React, Redux, Angular, GraphQL, MongoDB, and more. When I am not working, you will find me working and contributing to multiple open source projects (GitHub: github.com/alexknipfer) or getting some late night chess games in on Chess.com!"
+        description="Developer that specializes in building web applications using tools such as React, Redux, Angular, GraphQL, MongoDB, and more. When I am not working, you will find me working and contributing to multiple open source projects (GitHub: github.com/alexknipfer) or getting some late night chess games in on Chess.com!"
         url="https://alexknipfer.com"
       />
       <section className="mb-10">
