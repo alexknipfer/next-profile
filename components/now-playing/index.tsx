8f7f90500a6b6764d59d55ccb26d663f2d4c6452
alexknipfer <alexanderknipfer@gmail.com>
2021-02-15 22:14:45 -0500

    Show not playing state when spotify isn't actively playing

--- components/now-playing/index.tsx
+++ components/now-playing/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import useSWR from 'swr';
 
 import { NowPlayingResponse } from '../../pages/api/now-playing';
@@ -5,33 +6,55 @@ import { NowPlayingResponse } from '../../pages/api/now-playing';
 const NowPlaying: React.FC = () => {
   const { data } = useSWR<NowPlayingResponse>('/api/now-playing');
 
-  if (!data || !data.isPlaying) {
-    return null;
-  }
+  const isPlaying = data && data.isPlaying;
+
+  console.log('IS PLAYING: ', isPlaying);
+  // if (!data || !data.isPlaying) {
+  //   return null;
+  // }
 
   return (
     <div className="relative border border-card-border rounded-md flex items-center w-64 p-3 shadow">
-      <img
-        src={data.albumImage}
-        className="rounded block w-1/4"
-        alt="Spotify album cover"
-      />
-      <img
-        src="/static/images/spotify_icon.svg"
-        width={15}
-        height={15}
-        style={{ position: 'absolute', top: 8, right: 8 }}
-      />
-      <div className="w-3/4 pl-3">
-        <a
-          href={data.songUrl}
-          target="_blank"
-          rel="noreferrer"
-          className="font-bold truncate block hover:underline text-sm"
-        >
-          {data.songName}
-        </a>
-        <div className="truncate">{data.artists}</div>
+      {isPlaying ? (
+        <Image
+          src={data.albumImage}
+          width={57}
+          height={57}
+          className="rounded block"
+          alt={`Spotify album cover for ${data.artists}`}
+        />
+      ) : (
+        <Image
+          src="/static/images/spotify.png"
+          width={30}
+          height={30}
+          alt="Spotify music logo"
+        />
+      )}
+      {isPlaying && (
+        <img
+          src="/static/images/spotify.png"
+          width={15}
+          height={15}
+          className="absolute top-2 right-2"
+        />
+      )}
+      <div className="w-3/4 pl-4">
+        {isPlaying ? (
+          <a
+            href={data.songUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="font-bold truncate block hover:underline text-sm"
+          >
+            {data.songName}
+          </a>
+        ) : (
+          'Spotify'
+        )}
+        <div className="truncate">
+          {isPlaying ? data.artists : 'Not currently playing'}
+        </div>
       </div>
     </div>
   );
