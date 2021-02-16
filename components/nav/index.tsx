df9e75f22e9b433b9465239118c31fdc8f3a9802
alexknipfer <alexanderknipfer@gmail.com>
2021-02-15 22:33:02 -0500

    Add blurred nav

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -27,7 +27,7 @@ const Nav: React.FC = () => {
   const DarkModeSwitchIcon = isDarkTheme ? SunIcon : MoonIcon;
 
   return (
-    <nav className="sticky top-0 z-20 h-16 max-w-screen-md mx-auto px-4 md:px-8 my-8 bg-white dark:bg-gray-900">
+    <nav className="nav-backdrop sticky top-0 z-20 max-w-screen-md mx-auto px-4 md:px-8 my-8 bg-white dark:bg-gray-900 bg-opacity-80">
       <div
         className="z-20 md:hidden"
         onClick={() => setMenuVisible(!isMenuVisible)}
@@ -35,11 +35,11 @@ const Nav: React.FC = () => {
         <MenuIcon isOpen={isMenuVisible} />
       </div>
       <Menu isOpen={isMenuVisible} />
-      <div className="flex justify-between items-center w-full">
+      <div className="flex justify-between items-center w-full h-16">
         <div>
           {navItems.map(({ path, name }) => (
             <Link key={path} href={path}>
-              <a className="hidden md:inline p-4 text-base hover:bg-gray-100 dark:hover:bg-gray-800 rounded text-gray-700 dark:text-white">
+              <a className="hidden md:inline p-4 text-base text-gray-700 dark:text-white">
                 {name}
               </a>
             </Link>
@@ -47,11 +47,16 @@ const Nav: React.FC = () => {
         </div>
         <button
           onClick={() => setTheme(isDarkTheme ? 'light' : 'dark')}
-          className="p-2 dark:text-gray-100 text-gray-800 bg-gray-100 dark:bg-gray-800 rounded-md"
+          className="p-2 dark:text-gray-200 text-gray-700 bg-gray-200 dark:bg-gray-700 rounded-md"
         >
           <DarkModeSwitchIcon className="fill-current" />
         </button>
       </div>
+      <style jsx>{`
+        .nav-backdrop {
+          backdrop-filter: saturate(180%) blur(20px);
+        }
+      `}</style>
     </nav>
   );
 };
