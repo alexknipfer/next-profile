b6f006af70617500d0457b5ff3bf4d80bbc68bd7
alexknipfer <alexanderknipfer@gmail.com>
2021-02-15 21:48:40 -0500

    Remove previous theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,26 +8,6 @@ module.exports = {
       fontSize: {
         tiny: '0.6rem',
       },
-      colors: {
-        navigation: 'var(--bg-background-navigation)',
-        'navigation-hover': 'var(--bg-background-navigation-hover)',
-        primary: 'var(--bg-background-primary)',
-        text: 'var(--bg-background-text)',
-        overlay: 'var(--bg-background-overlay)',
-        'project-card-shadow': 'var(--bg-project-card-shadow)',
-        'project-card-details': 'var(--bg-project-card-details)',
-        'card-border': 'var(--card-border)',
-        'horizontal-rule': 'var(--horizontal-rule)',
-        copy: {
-          heading: 'var(--text-copy-heading)',
-          body: 'var(--text-copy-body)',
-          link: 'var(--text-copy-link)',
-          'project-card': 'var(--text-copy-project-card)',
-        },
-      },
-      boxShadow: {
-        'card-shadow': 'var(--card-shadow)',
-      },
     },
   },
 };
