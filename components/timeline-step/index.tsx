e42b7fd0198d4bcef9c1686342fa61c1efac0dc1
Alex <alexanderknipfer@gmail.com>
2021-02-16 19:45:39 -0500

    Fix icon and paragraph alignment

--- components/timeline-step/index.tsx
+++ components/timeline-step/index.tsx
@@ -7,12 +7,16 @@ interface Props {
 
 const TimelineStep: React.FC<Props> = ({ name, description }) => {
   return (
-    <li className="mb-5 last:mb-0">
+    <li className="mb-5 last:mb-0 text-gray-700 dark:text-gray-200">
       <div className="flex items-center mb-2">
-        <CheckmarkIcon width={24} height={24} className="mr-2" />
-        <p className="font-bold text-copy-heading">{name}</p>
+        <CheckmarkIcon
+          width={24}
+          height={24}
+          className="mr-2 fill-current flex-shrink-0"
+        />
+        <p className="font-bold">{name}</p>
       </div>
-      <p className="ml-8 text-copy-body">{description}</p>
+      <p className="ml-8">{description}</p>
     </li>
   );
 };
