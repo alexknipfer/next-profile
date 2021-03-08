0ef6d2aa8a2117369b3509436341bc96d795bd95
alexknipfer <alexanderknipfer@gmail.com>
2021-03-07 21:18:31 -0500

    Add new proejct images

--- components/project-card/index.tsx
+++ components/project-card/index.tsx
@@ -19,20 +19,20 @@ const ProjectCard: React.FC<Props> = ({ name, link, img }) => {
     >
       <div
         className={classNames(
-          'absolute top-0 left-0 z-10 w-full h-full flex flex-col justify-items-end transition-opacity duration-200 rounded-md bg-black bg-opacity-40',
+          'absolute top-0 left-0 z-10 w-full h-full flex flex-col justify-items-end transition-opacity duration-200 rounded-md bg-black bg-opacity-40 truncate',
           {
             'opacity-0': !isOverlayVisible,
             'opacity-100': isOverlayVisible,
           },
         )}
       >
-        <div className="bg-black bg-opacity-70 mt-auto rounded-b-md flex items-center flex-col py-3 px-4">
+        <div className="bg-black bg-opacity-70 mt-auto rounded-b-md flex items-center flex-col py-3 px-4 text-center">
           <div className="font-thin text-gray-200">{name}</div>
           <a
             href={link}
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-400"
+            className="text-blue-400 block w-full truncate"
           >
             {link}
           </a>
@@ -41,8 +41,8 @@ const ProjectCard: React.FC<Props> = ({ name, link, img }) => {
       <NextImage
         src={img}
         alt={`${name} Project`}
-        width={500}
-        height={283}
+        width={400}
+        height={234}
         className="rounded-md"
         priority
       />
