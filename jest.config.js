88621cf9014f7297d0aef54b73bda349e226a74d
alexknipfer <alexanderknipfer@gmail.com>
2021-05-31 22:33:00 -0400

    Setup ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,22 @@
 module.exports = {
+  preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
   moduleNameMapper: {
     '@/components/(.*)': '<rootDir>/components/$1',
     '@/test-utils': '<rootDir>/utils/testUtils',
   },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
 };
