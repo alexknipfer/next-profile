86420b78344c9faaf35f44e1b1a0cdec782bd5c3
alexknipfer <alexanderknipfer@gmail.com>
2021-06-02 22:50:01 -0400

    Remove log

--- utils/testUtils.tsx
+++ utils/testUtils.tsx
@@ -3,8 +3,6 @@ import { render, RenderOptions } from '@testing-library/react';
 import { SWRConfig } from 'swr';
 
 const Wrapper: React.FC = ({ children }) => {
-  console.log('wrapper!!!!!');
-
   return <SWRConfig value={{ dedupingInterval: 0 }}>{children}</SWRConfig>;
 };
 
