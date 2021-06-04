1645fc3a0db3c7f418a3b399ff8af0fb4ab3e606
alexknipfer <alexanderknipfer@gmail.com>
2021-06-03 21:55:33 -0400

    Finish adding tests for now playing

--- components/NowPlaying/NowPlaying.test.tsx
+++ components/NowPlaying/NowPlaying.test.tsx
@@ -3,6 +3,7 @@ import NowPlaying from '@/components/NowPlaying';
 import { ImageProps } from 'next/image';
 import { rest, server } from '@/test/server';
 import { NowPlayingResponseFactory } from '@/test/factories/NowPlayingResponseFactory';
+import { NowPlayingResponse } from 'pages/api/now-playing';
 
 /**
  * Current workaround for next/image until finding a better way for handling
@@ -12,20 +13,86 @@ import { NowPlayingResponseFactory } from '@/test/factories/NowPlayingResponseFa
  */
 jest.mock('next/image', () => (props: ImageProps) => <img {...props} />);
 
-test('should render now playing', async () => {
-  const nowPlayingResponse = NowPlayingResponseFactory.build({
-    isPlaying: true,
+describe('successful response from now playing', () => {
+  let nowPlayingResponse: NowPlayingResponse;
+
+  beforeEach(() => {
+    nowPlayingResponse = NowPlayingResponseFactory.build({
+      isPlaying: true,
+    });
+
+    server.use(
+      rest.get('/api/now-playing', (req, res, ctx) =>
+        res(ctx.json(nowPlayingResponse)),
+      ),
+    );
   });
 
-  server.use(
-    rest.get('/api/now-playing', (req, res, ctx) =>
-      res(ctx.json(nowPlayingResponse)),
-    ),
-  );
+  test('displaying name of song link', async () => {
+    render(<NowPlaying />);
 
-  render(<NowPlaying />);
+    const songAnchorElement = await screen.findByRole('link', {
+      name: nowPlayingResponse.songName,
+    });
 
-  const songNameElement = await screen.findByText(nowPlayingResponse.songName);
+    expect(songAnchorElement).toHaveTextContent(nowPlayingResponse.songName);
+    expect(songAnchorElement).toHaveAttribute(
+      'href',
+      nowPlayingResponse.songUrl,
+    );
+  });
 
-  expect(songNameElement).toBeInTheDocument();
+  test('song link should open in new tab', async () => {
+    render(<NowPlaying />);
+
+    const songAnchorElement = await screen.findByRole('link', {
+      name: nowPlayingResponse.songName,
+    });
+
+    expect(songAnchorElement).toHaveAttribute('target', '_blank');
+    expect(songAnchorElement).toHaveAttribute('rel', 'noreferrer');
+  });
+
+  test('displays artist(s) name', async () => {
+    render(<NowPlaying />);
+
+    const artistElement = await screen.findByText(nowPlayingResponse.artists);
+
+    expect(artistElement).toBeInTheDocument();
+  });
+});
+
+describe('song is not currently playing', () => {
+  let nowPlayingResponse: NowPlayingResponse;
+
+  beforeEach(() => {
+    nowPlayingResponse = NowPlayingResponseFactory.build({
+      isPlaying: false,
+    });
+
+    server.use(
+      rest.get('/api/now-playing', (req, res, ctx) =>
+        res(ctx.json(nowPlayingResponse)),
+      ),
+    );
+  });
+
+  test('displays default spotify logo', async () => {
+    render(<NowPlaying />);
+
+    const spotifyLogoElement = await screen.findByAltText('Spotify music logo');
+
+    expect(spotifyLogoElement).toHaveAttribute(
+      'src',
+      '/static/images/spotify.png',
+    );
+  });
+
+  test('displays `Not currently playing`', async () => {
+    render(<NowPlaying />);
+
+    const notPlayingElement = await screen.findByText('Not currently playing');
+
+    expect(notPlayingElement).toBeInTheDocument();
+  });
 });
